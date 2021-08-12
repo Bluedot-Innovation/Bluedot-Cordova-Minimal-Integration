7b347f450a2f83db84f992bd847b81cf337aa28d
Neha <neha@bluedot.io>
2021-08-12 12:02:37 +1000

    arrow function replaced by traditional function format

--- www/js/bdFunctions.js
+++ www/js/bdFunctions.js
@@ -11,7 +11,7 @@
  var projectId = "YOUR_PROJECT_ID";
  var destinationId = "";
  var orderId = "";
- 
+
  const CLAuthorizationStatusEnum =
  {
      notDetermined : 0,
@@ -28,7 +28,7 @@
          4: { name: "authorizedWhenInUse" }
      }
  }
- 
+
  const CLAccuracyAuthorizationEnum =
  {
      fullAccuracy : 0,
@@ -39,7 +39,7 @@
          1: { name: "reducedAccuracy" }
      }
  }
- 
+
  /*
   *  Add text to the status area.
   */
@@ -57,11 +57,11 @@
                                                 hourCycle: 'h23'
                                              }
                                             ).format(new Date())
-     
+
      textAreaId.value += timestamp + ": " +  statusText + "\n";
      textAreaId.scrollTop = textAreaId.scrollHeight
  }
- 
+
  function initializationSuccessfulCallback()
  {
      updateStatus( "Initialization Successful" );
@@ -74,17 +74,17 @@
              }, null, cordova.plugins.diagnostic.locationAuthorizationMode.ALWAYS);
      }
  }
- 
+
  function installRefCallback( installRef )
  {
      updateStatus("InstallRef: " + installRef);
  }
- 
+
  function sdkVersionCallback( sdkVersion )
  {
      updateStatus("Bluedot SDK Version: " + sdkVersion);
  }
- 
+
  /*
   *  Initialize Bluedot Point SDK
   */
@@ -92,80 +92,80 @@
  {
      updateStatus( "Initializing with Bluedot SDK..." );
      projectId = document.getElementById( "projectId" ).value;
-     
+
      // Display the installRef, SDK Version
      io.bluedot.cordova.plugin.getInstallRef(installRefCallback);
      io.bluedot.cordova.plugin.getSdkVersion(sdkVersionCallback);
-     
+
      //  Add the BluedotServiceDelegate functions for receiving data
      io.bluedot.cordova.plugin.bluedotServiceDidReceiveErrorCallback( bluedotServiceReceivedError );
      io.bluedot.cordova.plugin.locationAuthorizationDidChangeCallback( locationAuthorizationChanged );
      io.bluedot.cordova.plugin.accuracyAuthorizationDidChangeCallback( accuracyAuthorizationChanged );
      io.bluedot.cordova.plugin.lowPowerModeDidChangeCallback( lowPowerModeChanged );
-     
+
      //  Add the GeoTriggeringEventDelegate functions for receiving data
      io.bluedot.cordova.plugin.zoneInfoUpdateCallback( zoneUpdate );
      io.bluedot.cordova.plugin.enteredZoneCallback( zoneEntered );
      io.bluedot.cordova.plugin.exitedZoneCallback( zoneExited );
-     
+
      //  Add the TempTrackingDelegate functions for receiving data
      io.bluedot.cordova.plugin.tempoStoppedWithErrorCallback(
-         (error) => updateStatus("Tempo stopped with error: " + error)
+         function (error) { updateStatus("Tempo stopped with error: " + error) }
      );
      io.bluedot.cordova.plugin.tempoTrackingExpiredCallback( tempoTrackingExpired );
-     
+
      // Initialize SDK
      io.bluedot.cordova.plugin.initializeWithProjectId(
          initializationSuccessfulCallback,
-         (error) => updateStatus("Initialization Failed with error: " + error),
+         function (error) { updateStatus("Initialization Failed with error: " + error)},
          projectId);
  }
- 
+
  function doIsInitialized()
  {
      io.bluedot.cordova.plugin.isInitialized(
-         (isInitialized) => updateStatus("Is SDK Initialized: " + isInitialized)
+         function (isInitialized) { updateStatus("Is SDK Initialized: " + isInitialized) }
       );
  }
- 
+
  function doReset()
  {
      io.bluedot.cordova.plugin.reset(
-         () => updateStatus("Reset Successful"),
-         (error) => updateStatus("Reset Failed with error: " + error)
+         function () { updateStatus("Reset Successful") },
+         function (error) { updateStatus("Reset Failed with error: " + error)}
      );
  }
- 
- 
+
+
  function bluedotServiceReceivedError(error)
  {
      updateStatus(error);
  }
- 
+
  function locationAuthorizationChanged(previousStatus, newStatus)
  {
      updateStatus("Location Authorization Status: " + CLAuthorizationStatusEnum.properties[ newStatus ].name);
  }
- 
+
  function accuracyAuthorizationChanged(previousStatus, newStatus)
  {
      updateStatus("Accuracy Authorization Status: " + CLAccuracyAuthorizationEnum.properties[ newStatus ].name);
  }
- 
+
  function lowPowerModeChanged(isLowPowerMode)
  {
      updateStatus("Low Power Mode changed to " + isLowPowerMode);
  }
- 
+
  function doStartGeoTriggering()
  {
     updateStatus("Starting GeoTriggering");
-    
+
     if (device.platform === "iOS") {
         updateStatus("Starting iOS GeoTriggering...");
         io.bluedot.cordova.plugin.iOSStartGeoTriggering(
-            () => updateStatus("Start Geotriggering Successful"),
-            (error) => updateStatus("Start Geotriggering Failed with error: " + error)
+            function () { updateStatus("Start Geotriggering Successful") },
+            function (error) { updateStatus("Start Geotriggering Failed with error: " + error) }
         );
     } else if (device.platform === "Android") {
         console.log("Starting Android GeoTriggering...");
@@ -180,8 +180,8 @@
           };
 
           io.bluedot.cordova.plugin.androidStartGeoTriggering(
-            () => updateStatus("Start Geotriggering Successful"),
-            (error) => updateStatus("Start Geotriggering Failed with error: " + error),
+            function () { updateStatus("Start Geotriggering Successful") },
+            function (error) { updateStatus("Start Geotriggering Failed with error: " + error) },
             androidNotificationParams.channelId,
             androidNotificationParams.channelName,
             androidNotificationParams.title,
@@ -190,13 +190,13 @@
         );
     }
  }
- 
+
  function zoneUpdate( zoneInfos )
  {
      updateStatus( "Zones info has been updated for " + zoneInfos.length + " zones" );
      updateStatus( JSON.stringify(zoneInfos) );
  }
- 
+
  function zoneEntered( fenceInfo, zoneInfo, locationInfo, willCheckOut, customData )
  {
      updateStatus( fenceInfo["name"] + " has been triggered in " + zoneInfo["name"] + " at " + locationInfo["latitude"] + ":" + locationInfo["longitude"] );
@@ -211,48 +211,48 @@
          updateStatus( "Zone is awaiting check-out" );
      }
  }
- 
+
  function zoneExited( fenceInfo, zoneInfo, date, dwellTime, customData )
  {
      updateStatus( fenceInfo["name"] + " has been left in " + zoneInfo["name"] + " after " + dwellTime + " minutes" );
-     
+
      if(customData)
      {
         updateStatus( JSON.stringify(customData) );
      }
  }
- 
+
  function doStopGeoTriggering()
  {
      updateStatus("Stopping GeoTriggering...");
      io.bluedot.cordova.plugin.stopGeoTriggering(
-         () => updateStatus("Stop Geotriggering Successful"),
-         (error) => updateStatus("Stop Geotriggering Failed with error: " + error)
+         function () { updateStatus("Stop Geotriggering Successful") },
+         function (error) { updateStatus("Stop Geotriggering Failed with error: " + error) }
      );
  }
- 
+
  function doIsGeoTriggeringRunning()
  {
      io.bluedot.cordova.plugin.isGeoTriggeringRunning(
-         (isRunning) => updateStatus("Is Geo Triggering Running: " + isRunning)
+         function (isRunning) { updateStatus("Is Geo Triggering Running: " + isRunning) }
      );
  }
- 
+
  function doGetZones()
  {
      io.bluedot.cordova.plugin.getZones( zonesCallback );
  }
- 
+
  function zonesCallback(zoneInfos)
  {
      updateStatus( "Current zones count: " + zoneInfos.length + " zones" );
      console.log(JSON.stringify(zoneInfos));
  }
- 
+
  function doStartTempoTracking()
  {
      updateStatus("Starting Tempo...");
-     
+
      destinationId = document.getElementById( "destinationId" ).value;
      orderId = document.getElementById( "orderId" ).value;
 
@@ -261,12 +261,12 @@
      {
         io.bluedot.cordova.plugin.setCustomEventMetaData( { "hs_OrderId": orderId } )
         updateStatus( "Set CustomEventMetadata { \"hs_OrderId\": \"" + orderId + "\" }" );
-     }  
+     }
 
      if (device.platform === "iOS") {
         io.bluedot.cordova.plugin.iOSStartTempoTracking(
-            () => updateStatus("Start Tempo Successful"),
-            (error) => updateStatus("Start Tempo Failed: " + error),
+            function () { updateStatus("Start Tempo Successful") },
+            function (error) { updateStatus("Start Tempo Failed: " + error) },
             destinationId);
      } else if (device.platform === "Android") {
         const androidNotificationParams = {
@@ -278,8 +278,8 @@
         }
 
         io.bluedot.cordova.plugin.androidStartTempoTracking(
-            () => updateStatus("Start Tempo Successful"),
-            (error) => updateStatus("Start Tempo Failed: " + error),
+            function () { updateStatus("Start Tempo Successful") },
+            function (error) { updateStatus("Start Tempo Failed: " + error) },
             destinationId,
             androidNotificationParams.channelId,
             androidNotificationParams.channelName,
@@ -289,24 +289,24 @@
         );
     }
  }
- 
+
  function doStopTempoTracking()
  {
      updateStatus("Stopping Tempo...");
      io.bluedot.cordova.plugin.stopTempoTracking(
-         () => updateStatus("Stop Tempo Successful"),
-         (error) => updateStatus("Stop Tempo Failed: " + error)
+         function () { updateStatus("Stop Tempo Successful") },
+         function (error) { updateStatus("Stop Tempo Failed: " + error) }
      );
  }
- 
- 
+
+
  function doIsTempoRunning()
  {
      io.bluedot.cordova.plugin.isTempoRunning(
-         (isRunning) => updateStatus("Is Tempo Running: " + isRunning)
+     function (isRunning) { updateStatus("Is Tempo Running: " + isRunning) }
      );
  }
- 
+
  function tempoTrackingExpired()
  {
      updateStatus("Tempo Tracking Expired");
